755b030cade99ee9e7251177fa451e4a0f7023f7
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2026-02-08 06:52:30 +0600

    module

---
+++ module/Cal.js
@@ -0,0 +1,19 @@
+let addData=(num1, num2)=>{
+    return num1+num2
+}
+
+let minData=(num1, num2)=>{
+    return num1-num2
+}
+
+let mulData=(num1, num2)=>{
+    return num1*num2
+}
+
+let divData=(num1, num2)=>{
+    return num1/num2
+}
+
+export let user = ['abc', 'def', 'ghi'];
+
+export {addData, minData, mulData, divData}
---
+++ module/index.js
@@ -0,0 +1,6 @@
+// Import | Export 
+import { addData, user } from "./Cal.js";
+
+console.log(addData(10, 20));
+
+console.log(user);

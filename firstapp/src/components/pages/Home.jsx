e9e061585a73aff827e1ab04c3c050f05f25e7d5
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-02-18 19:21:03 +0600

    a

--- firstapp/src/components/pages/Home.jsx
+++ firstapp/src/components/pages/Home.jsx
@@ -9,15 +9,20 @@ function Home() {
         <section>
         <Header />
         <h1>Home</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, rerum nostrum? Cupiditate quam officiis quo, ducimus consequatur sed neque nulla facere! Iusto earum magni, doloribus eius excepturi eos reprehenderit suscipit!.</p>
         </section>
         <section className="productSection">
             oure products
             <div className="productMid">
-                <div className="productItems">
-                    <img src="/images/react.jpg" alt="" />
-                    <h3>Product Name</h3>
-                </div>
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
             </div>
         </section>
         </>
@@ -25,3 +30,14 @@ function Home() {
     )
 }
 export default Home
+
+function ProductCard(){
+    return (
+        <>
+        <div className="productItems">
+                    <img src="/images/react.jpg" alt="" />
+                    <h3>Product Name</h3>
+                </div>
+        </>
+    );
+}

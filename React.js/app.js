c060a6180cb190dd9b1ba9b1a57b6548cd4f709c
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-02-19 16:02:42 +0600

    add

---
+++ React.js/app.js
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+
+function Car(props) {
+  return (
+    <>
+      <h2>My {props.carinfo.name} {props.carinfo.model}!</h2>
+      <p>It is {props.carinfo.color} and it is from {props.carinfo.year}!</p>
+    </>
+  );
+}
+
+const carInfo = {
+  name: "Ford",
+  model: "Mustang",
+  color: "red",
+  year: 1969
+};
+
+createRoot(document.getElementById('root')).render(
+  <Car carinfo={carInfo} />
+);

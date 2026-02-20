fa7cc72c5aae00b4435b08af39fbee0bfb930662
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-02-20 15:56:16 +0600

    a

---
+++ newapp/src/components/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Home() {
+
+    let count = 0
+    return (
+
+        <section>
+
+            <h1 className='text-center text-4xl font-bold py-20'>Celebration wear for me</h1>
+            <p className='text-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. 
+            </p>
+
+            <div className='max-w-[1320px] mx-auto mt-5 grid grid-cols-4 gap-5'>
+                 <ProductCard />   
+            </div>  
+
+        </section>
+  )
+}
+
+function ProductCard(){
+    <div>
+        fsssssssssssssssssssssfs
+    </div>
+}
--- newapp/src/main.jsx
+++ newapp/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css' 
+import Home from './components/pages/Home'
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
+    <Home/>
+    <Footer/>
   </StrictMode>,
 )
